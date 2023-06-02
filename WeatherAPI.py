ea299fbf7bd1a4f6083488eb538be28f71d3d7f6
p2wna22005 <126845813+p2wna22005@users.noreply.github.com>
2023-06-02 17:31:32 +0530

    Update WeatherAPI.py

--- WeatherAPI.py
+++ WeatherAPI.py
@@ -2,7 +2,7 @@ import json
 from urllib.request import urlopen
 #Create user account and obtain API key from https://www.weatherapi.com
 
-url = "https://api.weatherapi.com/v1/current.json?key=f0b7067058b3405dbe641813232905=kollam&aqi=no"
+url = "https://api.weatherapi.com/v1/current.json?key=746c5ad6052440c780040657232905&q=kollam&aqi=no"
 
 api_page = urlopen(url)
 api=api_page.read()
